431dc2b5ad630aebe377ea21abda044da0d351ab
Bartłomiej Bajda <allerbbb@gmail.com>
2018-02-04 09:56:42 +0000

    update project

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function drawTree(height) {
+  for(var i = 1; i <= height; i++) {
+    var space= "                   " ;
+    var star ="";
+    for (var j = 0; j < i; j++){
+      var str2 = space.substring(1,(height-j));
+      star += '*';
+    } console.log(str2 + star + star);
+  }
+}
+
+drawTree(15);
